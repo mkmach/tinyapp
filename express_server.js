6a550c8dec011f0abb610f7b4a6b07a74d5e12bb
mkmach <mkmach@hotmail.com>
2022-10-04 16:07:20 -0700

    post request added

--- express_server.js
+++ express_server.js
@@ -8,6 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
 
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,6 +21,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -51,3 +57,5 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ 
